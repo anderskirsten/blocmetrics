363f24a43fd5b4e145d8bfbfe1a60045533164d2
Kirsten Anderson <anders.kirsten@gmail.com>
2017-11-01 18:10:14 +0000

    User Signup/Signin: Complete

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,5 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  config.action_mailer.default_url_options = { host: 'secure-stream-82670.herokuapp.com/' }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  
+  devise_for :users
+  
   get 'about' => 'welcome#about'
   
   get 'welcome' => 'welcome#index'
